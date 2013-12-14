fc5e302a0ffa79d50caf091f967059a74865339e
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2013-12-14 21:49:06 +0100

    Added ploting script for iterator benchmarks

--- src/scripts/iterator_benchmark_run.sh
+++ src/scripts/iterator_benchmark_run.sh
@@ -8,13 +8,19 @@ echo "darray - iterator access"
 $CMD -r$NRUNS -tdarray &> iterator_darray.dat
 echo "darray view - iterator acess"
 $CMD -r$NRUNS --view -tdarray &> iterator_darray_view.dat
+echo "darray - pointer access"
+$CMD -r$NRUNS -p -tdarray &> pointer_darray.dat
 
 echo "farray - iterator access"
 $CMD -r$NRUNS -tfarray &> iterator_farray.dat
 echo "farray view - iterator access"
 $CMD -r$NRUNS --view -tfarray &> iterator_farray_view.dat
+echo "farray - pointer access"
+$CMD -r$NRUNS -p -tfarray &> pointer_farray.dat
 
 echo "sarray - iterator access"
 $CMD -r$NRUNS -tsarray &> iterator_sarray.dat
 echo "sarray view - iterator access"
 $CMD -r$NRUNS --view -tsarray &> iterator_sarray_view.dat
+echo "sarray - pointer access"
+$CMD -r$NRUNS -p -tsarray &> pointer_sarray.dat
---
+++ src/scripts/iterator_eval.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import numpy
+from matplotlib import pyplot 
+import sys
+from itertools import chain
+import re
+from benchmark import get_result
+from benchmark import get_result_range
+
+import argparse
+
+def plot_hists(ax,result):
+
+    legend_list = []
+    for i in result.data.items():
+        ax.hist(i[1],bins=100,log=False)
+        legend_list.append(i[0])
+
+    pyplot.legend(legend_list)
+
+
+parser = argparse.ArgumentParser(description="Command line options")
+parser.add_argument('--ptr-data','-p',
+                    type=str,
+                    action="store",
+                    dest="pointer_file",
+                    help="file with pointer access data")
+parser.add_argument('--iter-data','-i',
+                    type=str,
+                    action="store",
+                    dest="iterator_file",
+                    help="file with iterator access data")
+parser.add_argument('--view-data','-v',
+                    type = str,
+                    action = "store",
+                    dest = "view_file",
+                    help = "file with view iterator access data")
+parser.add_argument('--bins','-b',
+                    type = int,
+                    action = "store",
+                    dest = "nbins",
+                    default = 10,
+                    help = "number of bins for the histogram")
+parser.add_argument('--logx',action="store_true",
+                    dest="log_x",
+                    help="plot x-axis on logarithmic scale")
+
+args = parser.parse_args()
+
+iter_result = get_result(args.iterator_file)
+ptr_result  = get_result(args.pointer_file)
+view_result = get_result(args.view_file)
+
+nbins = args.nbins;
+
+pyplot.figure()
+ax = pyplot.subplot(111)
+ax.hist(iter_result.data["write"],nbins,log=True,color='r')
+ax.hist(ptr_result.data["write"],nbins,log=True,color='g')
+ax.hist(view_result.data["write"],nbins,log=True,color='b')
+
+if args.log_x:
+    ax.set_xscale('log')
+
+pyplot.show()
+
